4a528ca9d7899b86834604c04113e7d4016bf595
erika <kekakooelhoo1@gmail.com>
2026-05-13 11:37:23 -0300

    add info no form e estilizei no css

---
+++ script.js
@@ -0,0 +1,37 @@
+const input = document.getElementById("valor")
+const btn_enviar = document.getElementById("btn-enviar")
+const paragrafo = document.querySelector(".paragrafo")
+
+
+function verificarTemperatura() {
+
+    const temperatura = Number(input.value)
+    
+    if( input.value === ""){
+        paragrafo.textContent = "Informe um valor!"
+         paragrafo.style.color = "blue"
+           return
+    }
+
+    if (temperatura > 90) {
+         paragrafo.textContent =" ⚠️ATENÇÃO ⚠️ TEMPERATURA ELEVADA."
+         paragrafo.style.color = "red"
+         paragrafo.style.fontSize= "13.5px"
+
+    } else {
+
+        paragrafo.textContent =
+        "Temperatura normal, pode operar a máquina normalmente.✅"
+
+        paragrafo.style.color = "green"
+    }
+}
+
+btn_enviar.addEventListener("click", (e) => {
+
+    e.preventDefault()
+
+    btn_enviar.style.color = "red"
+
+    verificarTemperatura()
+})
